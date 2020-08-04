4d7b5ea2467dc7471139025539fa8b5c5463736b
Piyush Mandloi <piyush.mandloi@zensar.com>
2020-08-04 09:37:01 +0530

    First Commit

---
+++ VehicleApp/VehicleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sample;
+
+namespace VehicleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle("Swift", "Black");
+            car.Start(car.VehTypes);
+            //car.Stop(car.VehTypes);
+
+
+
+            Vehicle truck = new Vehicle("Eicher", "Red", VehType.truck, 6, FuelType.diesel);
+            truck.Start(truck.VehTypes);
+            //truck.Stop(truck.VehTypes);
+
+
+
+            Vehicle smallcar = new Vehicle("Nano", "silver", VehType.car, 4);
+            smallcar.Start(smallcar.VehTypes);
+            //smallcar.Stop(smallcar.VehTypes);
+        }
+    }
+}
